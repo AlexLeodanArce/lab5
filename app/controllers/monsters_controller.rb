6695cd2d352fc6f0f6fbe3b782861235cc4f470e
AlexLeodanArce <alexcanada201022@gmail.com>
2022-10-24 02:32:40 +0000

    Monster creation available

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -6,4 +6,23 @@ class MonstersController < ApplicationController
   def show
     @monster = Monster.find(params[:id])
   end
+
+  def new
+    @monster= Monster.new
+  end
+
+  def create
+    @monster= Monster.new(monster_params)
+    if @monster.save
+      redirect_to monster_path(@monster)
+    else
+      render :new, status: :see_other
+    end
+  end
+
+private
+
+  def monster_params
+    params.require(:monster).permit(:name,:description,:phone,:birthdate)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :monsters, only: [:index]
+  resources :monsters, only: [:index, :show, :new, :create, :edit, :update]
 
   root "monsters#index"
 end
